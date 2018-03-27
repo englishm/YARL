7eb9628c2a0fc1e5d4de7011ae1159cd16479a85
ntoskrnl4 <gogadgetmatthew@gmail.com>
2018-03-27 01:20:30 +0000

    write docstring for exports module

---
+++ exports/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module contains exporters for various common formats.
+How the API will look is still being discussed in #dev, and will probably get turned into an issue.
+
+These modules have a dream, that one day, they will be able to import files along with export them.
+(But that day is not now, unfortunately. Sorry.)
+"""
+
+__author__ = "ntoskrnl4"
+
