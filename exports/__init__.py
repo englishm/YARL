466a80f411e8b19b8dde760514b2da36feb46d0b
ntoskrnl4 <gogadgetmatthew+github@gmail.com>
2018-03-28 21:44:06 +0000

    added .adx file export class

--- exports/__init__.py
+++ exports/__init__.py
@@ -5,5 +5,3 @@ How the API will look is still being discussed in #dev, and will probably get tu
 These modules have a dream, that one day, they will be able to import files along with export them.
 (But that day is not now, unfortunately. Sorry.)
 """
-
-__author__ = "ntoskrnl4"
---
+++ exports/adx.py
@@ -0,0 +1,103 @@
+"""
+This module provides a class that handles exporting a database to .adx format.
+"""
+
+import os
+from datetime import datetime
+from xml.etree.ElementTree import Element, ElementTree  # this should be all we need
+
+
+def _add_tag(parent, name, data=None, attributes={}):
+    """
+    Function to add a new element to a parent element.
+
+    :param parent: Tag to add subtag to
+    :param name: Name of the new element
+    :param data: Content of the new element (default None)
+    :param attributes: Dictionary of attributes of the new element (default {})
+    :return: The newly created element
+    """
+    new_tag = Element(name, attributes)
+    if data is not None:
+        new_tag.text = data
+    parent.append(new_tag)
+    return new_tag
+
+
+class ADIFExportError(Exception):
+    """Raised when there is an error exporting to an .adx file."""
+
+
+class ADXFile:
+    """
+    This class provides an API for exporting QSO information to a .adx file.
+
+    This class provides 3 functions that can be called to add QSO information to a .adx file. The file that will be
+    written to is passed in as a path during instantiation. The class checks if the path is OK to use, and if it is,
+    you can then use
+    """
+
+    def __init__(self, file):
+        """
+        Create a new ADXFile that can be used to export QSOs into ADIF (specifically, ADX files).
+
+        :param file: Location to create file at.
+        :return: None
+        """
+        # we're going to throw an error if the location to export to already exists. It shouldn't, if the GUI is done
+        # right, but we're going to anyway for posterity.
+        if not os.path.exists(file):
+            self.location = file
+        else:
+            raise FileExistsError(f"The location you'd like to export to already exists. (got: {file})")
+
+        self._tree = ElementTree(Element("ADX"))  # add the root element, <ADX>
+        self._root = self._tree.getroot()  # use _add_tag() to add a subtag to it in a nicer format
+        self._in_records = False  # we are going to check these two variables to see if we are currently writing records
+        self._records = None  # and what the main records tag is
+        self._header_written = False
+
+    def write_header(self, _adif_ver="3.0.8", _app_name="YARL"):
+        """
+        Write the header for an .adx file.
+
+        :param _adif_ver: ADIF version. Default and currently supported version is "3.0.8"
+        :param _app_name: Name of the application. Default "YARL"
+        :return: None
+        :raise: ADIFExportError if called twice.
+        """
+        if self._header_written:
+            raise ADIFExportError("Attempted adding header to file which already had a header")
+        if self._in_records:
+            raise ADIFExportError("Cannot add header while we're already writing records")
+            # Technically, it is possible, but it is not defined in the ADIF documentation if it can be added to the end
+            # (which is where it would be added)
+
+        header = _add_tag(self._root, "HEADER")
+        _add_tag(header, "ADIF_VER", _adif_ver)
+        _add_tag(header, "PROGRAMID", _app_name)
+        _add_tag(parent=header,
+                 name="APP",
+                 data=datetime.now().__str__(),
+                 attributes={
+                    "PROGRAMID": _app_name,
+                    "FIELDNAME": "GEN_TIME",  # time this file was written
+                    "TYPE": "S",  # S data type indicator for string
+                    })
+
+    def write_record(self, data: dict):
+        """Add a new record to the file to be exported."""
+        if not self._in_records and self._records is None:  # great, we're starting to write our records now!
+            self._records = _add_tag(self._root, "RECORDS")
+            self._in_records = True
+        current_record = _add_tag(self._records, "RECORD")
+        for key in data:  # next line: TODO: Add support for user and app fields and metadata
+            modified_key = key.upper().replace(" ", "_")
+            _add_tag(current_record, name=modified_key, data=data[key])
+
+    def write_file(self):
+        """Write the final tree to file."""
+        if os.path.exists(self.location):
+            raise FileExistsError(f"The location you'd like to export to already exists. (got: {file})")
+        self._tree.write(self.location)
+        return True
